48c55e1feea31303d80763a294db3dd991f92c13
Jinsi-S-J <121845984+Jinsi-S-J@users.noreply.github.com>
2023-02-14 10:00:19 +0530

    Reports

--- src/test/java/Day01/Program01.java
+++ src/test/java/Day01/Program01.java
@@ -1,9 +1,24 @@
 package Day01;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class Program01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// How Reports are generated using Maven
+		
+		ExtentReports report = new ExtentReports(".\\src\\test\\resources\\report\\Feb14.html");
+		ExtentTest test= report.startTest("TC01","Login with valid Username and password");
+		test.log(LogStatus.INFO, "Open ChromeBrowser");
+		test.log(LogStatus.INFO, "Open Application URL");
+		test.log(LogStatus.INFO, "Enter the User Name in text box");
+		test.log(LogStatus.INFO, "Enter the Password in text box");
+		test.log(LogStatus.INFO, "Click Log-in");
+		report.endTest(test);//saved testcases
+		report.flush();//saved report
+		
 
 	}
 
